fdea193c01a2a2796d87b442ade90d03f47d5159
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-09-29 20:16:56 -0300

    mapeando com fluent mapping

---
+++ Blog/Blog/Mappings/CategoryMap.cs
@@ -0,0 +1,47 @@
+﻿using Blog.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //Tabela
+            builder
+                .ToTable("Category");
+
+            //Chave primária
+            builder
+                .HasKey(x => x.Id);
+            
+            //Identity
+            builder
+                .Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            //Propriedades
+            builder
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder
+                .Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+
+            //Indices
+            builder
+                .HasIndex(x => x.Slug, "IX_Category_Slug")
+                .IsUnique();
+        }
+    }
+}
---
+++ Blog/Blog/Mappings/PostMap.cs
@@ -0,0 +1,106 @@
+﻿using Blog.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+
+namespace Blog.Mappings
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            // Tabela
+            builder
+                .ToTable("Post");
+
+            // Chave rimária
+            builder
+                .HasKey(x => x.Id);
+
+            // Identity
+            builder
+                .Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            // Propriedades
+            builder.
+                Property(x => x.Title)
+                .IsRequired()
+                .HasColumnName("Title")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder
+                .Property(x => x.Summary)
+                .IsRequired()
+                .HasColumnName("Summary")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder
+                .Property(x => x.Body)
+                .IsRequired()
+                .HasColumnName("Body")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder
+                .Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder
+                .Property(x => x.CreateDate)
+                .IsRequired()
+                .HasColumnName("CreateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValueSql("GETDATE()");
+
+            builder
+                .Property(x => x.LastUpdateDate)
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+            // Índices
+            builder
+                .HasIndex(x => x.Slug, "IX_Post_Slug")
+                .IsUnique();
+
+            // Relacionamentos
+            builder
+                .HasOne(x => x.Author)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Author")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasOne(x => x.Category)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Category")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTage",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade));
+        }
+    }
+}
