0cfa7c8547065ff38cb18625a2d7e60d171ea561
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-09-29 09:35:54 -0300

    update

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Blog
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
