52d840faf99b0ab64eb1655040e19601d76fad1c
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-09-29 18:02:31 -0300

    first crud

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+
+using Blog.Data;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
@@ -6,7 +11,48 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            
+            using (var _context = new BlogDataContext())
+            {
+                //var tag = new Tag
+                //{
+                //    Name = "ASP.NET",
+                //    Slug = "aspnet"
+                //};
+
+                //_context.Add(tag);
+                //_context.SaveChanges();
+
+                //var tag = _context.Tags.FirstOrDefault(x => x.Id == 1);
+                //tag.Name = ".NET";
+                //tag.Slug = "dotnet";
+
+                //_context.Update(tag);
+                //_context.SaveChanges();
+
+                //var tag = _context.Tags.FirstOrDefault(x => x.Id == 1);
+                //_context.Remove(tag);
+                //_context.SaveChanges();
+
+                //var tags = _context
+                //    .Tags
+                //    .AsNoTracking()
+                //    .Where(x => x.Name.Contains(".NET"))
+                //    .ToList();
+
+                //foreach (var tag in tags)
+                //{
+                //    Console.WriteLine(tag.Name);
+                //}
+
+                //var tag = _context
+                //    .Tags
+                //    .AsNoTracking()
+                //    .FirstOrDefault(x => x.Id == 3);
+                
+                //Console.WriteLine(tag?.Name);
+            }
+
+
         }
     }
 }
