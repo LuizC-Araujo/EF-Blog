96ddce8d19f981a142d2f6924f5c8eac317dad65
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-09-29 18:38:03 -0300

    trabalhando com include

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -13,36 +13,48 @@ namespace Blog
         {
             using var _context = new BlogDataContext();
 
-            var user = new User
-            {
-                Name = "Luiz Araujo",
-                Slug = "luizaraujo",
-                Email = "luiz@araujo.com",
-                Bio = "Dev",
-                Image = "https://image",
-                PasswordHash = "123456"
-            };
+            //var user = new User
+            //{
+            //    Name = "Luiz Araujo",
+            //    Slug = "luizaraujo",
+            //    Email = "luiz@araujo.com",
+            //    Bio = "Dev",
+            //    Image = "https://image",
+            //    PasswordHash = "123456"
+            //};
 
-            var category = new Category
-            {
-                Name = "Backend",
-                Slug = "backend"
-            };
+            //var category = new Category
+            //{
+            //    Name = "Backend",
+            //    Slug = "backend"
+            //};
 
-            var post = new Post
-            {
-                Author = user,
-                Category = category,
-                Body = "<h1>Asp Net</h1>",
-                Slug = "comecando-com-ef-Core",
-                Summary = "Vamos aprender EF Core",
-                Title = "Começando com EF Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now
-            };
+            //var post = new Post
+            //{
+            //    Author = user,
+            //    Category = category,
+            //    Body = "<h1>Asp Net</h1>",
+            //    Slug = "comecando-com-ef-Core",
+            //    Summary = "Vamos aprender EF Core",
+            //    Title = "Começando com EF Core",
+            //    CreateDate = DateTime.Now,
+            //    LastUpdateDate = DateTime.Now
+            //};
 
-            _context.Posts.Add(post);
-            _context.SaveChanges();
+            //_context.Posts.Add(post);
+            //_context.SaveChanges();
+
+            var posts = _context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToList();
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
+            }
         }
     }
 }
